b91feb444bfdcf73c3a2670b54393e03dfc13cb1
thuctapnhom02 <thuctapnhom02mock@gmail.com>
2024-10-29 14:23:12 +0700

    Delete BE_Bao_java_react directory

--- BE_Bao_java_react/test_2/BE_java_quanlychungcu/demo/src/main/java/com/example/demo/model/repo/PaymentMethodRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.model.repo;
-
-import com.example.demo.model.entity.PaymentMethodEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentMethodRepo extends JpaRepository<PaymentMethodEntity,String> {
-}
--- BE_Bao_java_react/test_2/BE_java_quanlychungcu/demo/src/main/java/com/example/demo/model/request/CreateRespone.java
+++
@@ -1,4 +0,0 @@
-package com.example.demo.model.request;
-
-public class CreateRespone {
-}
